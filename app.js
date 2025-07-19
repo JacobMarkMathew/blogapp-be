f4f9ef81ded99c16524eaa6e5d08d3874e152c05
jacob markmathew <jacobmathew00455@gmail.com>
2025-07-19 17:53:25 +0530

    back end

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const bcrypt=require("bcrypt")
+const cors=require("cors")
+const jsonwebtoken=require("jsonwebtoken")
+
+let app =express()
+
+
+app.get("/",(req,res)=>{
+    res.send("hello")
+})
+
+app.listen(3030,()=>{
+    console.log("server started")
+})

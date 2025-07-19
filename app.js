30f9387d662b4d8025441dd38cbc983782b60264
jacob markmathew <jacobmathew00455@gmail.com>
2025-07-19 19:47:13 +0530

    complete

--- app.js
+++ app.js
@@ -12,7 +12,38 @@ app.use(cors())
 
 mongoose.connect("mongodb+srv://jacobmarkmathew:jacobmark52@cluster0.w92biv9.mongodb.net/blogappDb?retryWrites=true&w=majority&appName=Cluster0")
 
+//sign in
+app.post("/sign In",async(req,res)=>{
+    let input=req.body
+    let result=userModel.find({email:req.body.email}).then((items)=>{
+        if(items.length>0){
 
+            const passwordValidator=bcrypt.compareSync(req.body.password,items[0].password)
+            if (passwordValidator){
+
+                jsonwebtoken.sign({email:req.body.email},"blogApp",{expiressIn:"1d"},(error,token){
+                    if(error){
+                        res.json({"status":"error"})
+                    }
+                    else{
+                        res.json({"status":"success","token":token,"userid":items[0].id})   
+                    }
+                })
+
+               
+            }
+            else{
+                res.json({"status":"success"})
+            }
+
+        }
+        else{
+            res.json({"status":"Invalid Email Id"})
+        }
+    }).catch()
+})
+
+//signup
 app.post("/signup",async(req,res)=>{
    
 let input=req.body
